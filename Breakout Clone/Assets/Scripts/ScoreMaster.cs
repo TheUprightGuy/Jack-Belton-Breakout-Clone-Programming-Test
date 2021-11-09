ecebfa00e9e4ae425cbbaa8a6ce316144ec16e54
Jack <jack.belton@hotmail.com>
2021-11-09 18:06:22 +1300

    Adding Polish

--- Breakout Clone/Assets/Scripts/ScoreMaster.cs
+++ Breakout Clone/Assets/Scripts/ScoreMaster.cs
@@ -16,6 +16,25 @@ public class ScoreMaster : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (EmptyCheck())
+        {
+            foreach (Transform item in transform)
+            {
+                item.gameObject.SetActive(true);
+            }
+        }
+    }
+
+    bool EmptyCheck()
+    {
+        foreach (Transform item in transform)
+        {
+            if (item.gameObject.activeInHierarchy)
+            {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
